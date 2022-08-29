a4cc8f62466c019a6a994b931298bbf541eaa230
mehranalam <mehranalambeigi@gmail.com>
2022-08-29 22:27:12 +0430

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ npm run build
 # navigate into the build output directory
 cd dist
 # if you are deploying to a custom domain# echo 'www.example.com' > CNAME
-git initgit add -A
+git add -A
 git commit -m 'deploy'
 git push -f git@github.com:Distro-ir/Distro.git master:gh-pages
 cd -
